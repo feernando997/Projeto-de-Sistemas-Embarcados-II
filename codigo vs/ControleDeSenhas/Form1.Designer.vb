aa5ef1474039e914338cd109548493342641d4c4
Lucas Fernando <lucasfernando0970@gmail.com>
2020-05-15 17:51:57 -0300

    Revert "Revert "Atualização visual Software, mudança de variaveis no pic 2""
    This reverts commit 67f7c1ed89ac597aa57935621f639c6bc64de0d5.

--- codigo vs/ControleDeSenhas/Form1.Designer.vb
+++ codigo vs/ControleDeSenhas/Form1.Designer.vb
@@ -23,6 +23,7 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.btnDecrPriorit = New System.Windows.Forms.Button()
         Me.btnDecrNormal = New System.Windows.Forms.Button()
@@ -676,7 +677,11 @@ Partial Class Form1
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
         Me.Name = "Form1"
+        Me.Text = "Controle de Senhas"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox2.ResumeLayout(False)
         Me.GroupBox3.ResumeLayout(False)
